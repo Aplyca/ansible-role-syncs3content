13b5a3847e83711afe7bfe137a20a38fcfbee924
Mauricio Sánchez <msanchez@aplyca.com>
2016-06-15 14:43:30 -0500

    Separate storage for To and From actions

--- tests/tests.sh
+++ tests/tests.sh
@@ -5,10 +5,10 @@ cd $(dirname "$0") && cd ..
 
 echo "Checking syntax"
 
-ansible-playbook -i inventories/local playbooks.yml --connection=local --extra-vars "action=False" --syntax-check
+ansible-playbook -i inventories/local playbooks.yml --connection=local --extra-vars "@tests/tests.yml" --syntax-check
 
 echo "Running role"
-ansible-playbook -i inventories/local playbooks.yml --connection=local --extra-vars "action=False"
+ansible-playbook -i inventories/local playbooks.yml --connection=local --extra-vars "@tests/tests.yml"
 
 echo "Checking idempotence"
-ansible-playbook -i inventories/local playbooks.yml --connection=local --extra-vars "action=False" | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
+ansible-playbook -i inventories/local playbooks.yml --connection=local --extra-vars "@tests/tests.yml" | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
